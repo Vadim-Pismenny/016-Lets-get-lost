2b78b93a5069e3dfa8394a2322b0a318219abc8c
Vadim-Pismenny <Vadim-Pismenny>
2018-09-18 14:06:16 +0400

    Ninja Vue js #7

---
+++ app.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+
+    },
+    methods: {
+      logName: function(){
+        console.log('you entered you name');
+      },
+      logAge: function(){
+        console.log('you entered you age');
+      }
+    }
+});
